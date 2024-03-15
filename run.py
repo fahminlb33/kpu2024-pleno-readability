6095601128f476c0bb636baefe5a41fbf6036659
fahminlb33 <fahminlb33@gmail.com>
2024-03-16 06:39:31 +0700

    feat: process through all pages

--- run.py
+++ run.py
@@ -1,13 +1,26 @@
 import csv
+import numpy as np
 from joblib import Memory, Parallel, delayed
 
 from model import Row
 from processors import measure_blur, measure_similarity, get_template, get_image
 
+TEMPLATE_PLENO_P1 = "dataset/benchmark/1101102001002_002_p1.jpg"
+TEMPLATE_PLENO_P2 = "dataset/benchmark/1101102001002_002_p2.jpg"
+TEMPLATE_PLENO_P3 = "dataset/benchmark/1101102001002_002_p3.jpg"
 
 def load_template(name: str):
     return get_template(name)
 
+def perform_measures(id: str, url: str, template: np.ndarray) -> str:
+    # download image
+    img = get_image(url)
+
+    # measure blur and similarity
+    laplacian_blur, fft_blur = measure_blur(img)
+    similarity = measure_similarity(img, template)
+
+    return f"{id},{laplacian_blur},{fft_blur},{similarity}\n"
 
 def main():
     # create cache
@@ -17,22 +30,19 @@ def main():
     load_template_cached = memory.cache(load_template)
 
     # image processor
-    def process(line: str):
+    def process(line: str) -> list[str]:
         try:
             # parse line
             row = Row.from_line(line)
             if row is None or not row.is_ok:
                 return None
 
-            # get pleno c1
-            img = get_image(row.chasil_hal_1)
-
             # measure blur and similarity
-            laplacian_blur, fft_blur = measure_blur(img)
-            similarity = measure_similarity(img, load_template_cached("dataset/1101102001002_002_p1.jpg"))
-
-            # print(f"Processed {row.kode} TPS {row.kode}")
-            return f"{row.kode},{row.tps},{laplacian_blur},{fft_blur},{similarity}\n"
+            return [
+                perform_measures(f"{row.kode},{row.tps},p1", row.chasil_hal_1, load_template_cached(TEMPLATE_PLENO_P1)),
+                perform_measures(f"{row.kode},{row.tps},p2", row.chasil_hal_2, load_template_cached(TEMPLATE_PLENO_P2)),
+                perform_measures(f"{row.kode},{row.tps},p3", row.chasil_hal_3, load_template_cached(TEMPLATE_PLENO_P3)),
+            ]
         except Exception as e:
             print(f"Error processing {line}: {e}")
             return None
@@ -46,13 +56,15 @@ def main():
         parallel = Parallel(n_jobs=-1, return_as="generator", verbose=10)
         output_gen = parallel(delayed(process)(line) for line in csvreader)
 
+        # write header
+        outfile.write("kode,tps,page,laplacian_blur,fft_blur,similarity\n")
+
         # write to file
-        for line in output_gen:
-            if line is None:
+        for out in output_gen:
+            if out is None:
                 continue
 
-            outfile.write(line)
-
+            outfile.writelines(out)
 
 
 if __name__ == '__main__':
